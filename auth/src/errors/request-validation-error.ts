e5346b2d614761f16df032a417f222af87d4101f
Perry <perry@jamdirect.ca>
2021-01-19 12:10:12 -0500

    Refactored error classes to extend custom error class. Added abstract custom error class.

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,17 +1,10 @@
 import { ValidationError } from 'express-validator'
-
-interface CustomError {
-    statusCode: number;
-    serializeErrors():{
-        message: string;
-        field?: string;
-    }[]
-}
+import { CustomError } from './custom-error';
 export class RequestValidationError extends Error implements CustomError{
     statusCode = 400;
 
     constructor(public errors: ValidationError[]) {
-        super();
+        super('Invalid request parameters');
 
         // Only because we are extending a built in class
         Object.setPrototypeOf(this, RequestValidationError.prototype)
