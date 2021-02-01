21fae29f15d721cb90e9e09ce146d2cb1a6d92c4
Perry <perry@jamdirect.ca>
2021-02-01 10:53:36 -0500

    Clean up of json view response directly in the mongoose User model.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -32,7 +32,11 @@ app.all('*', async (req, res) => {
 
 app.use(errorHandler)
 
-const startupDB = async() => {
+const start = async() => {
+    if (!process.env.JWT_KEY) {
+        throw new Error('JWT_KEY must be defined');
+    }
+    
     try {
         await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
             useNewUrlParser: true,
@@ -49,4 +53,4 @@ const startupDB = async() => {
     })
 }
 
-startupDB();
+start();
