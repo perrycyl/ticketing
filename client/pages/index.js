e8aa70e05d673d1e85af67a0984f6f336896e3dd
Perry <perry@jamdirect.ca>
2021-02-02 13:49:58 -0500

    Extracted current user query to build-client file for reuseability

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,23 +1,14 @@
-import axios from 'axios';
+import buildClient from "../api/build-client"
 
 const LandingPage = ({currentUser}) => {
+    console.log("current user: ", currentUser)
     return <h1 > Landing Page </h1>;
 }
 
-LandingPage.getInitialProps = async ({req}) => {
-    console.log("req", req.headers)
-    if (typeof window === 'undefined'){
-        const {data} = await axios.get(
-            'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', 
-            {
-                headers: req.headers
-            }
-        );
-        return data
-    } else {
-        const {data} = await axios.get('/api/users/currentuser');
-        return data
-    }
+LandingPage.getInitialProps = async (context) => {
+    const { data } = await buildClient(context).get('/api/users/currentuser')
+    console.log("DATA: ", data)
+    return data;
 };
 
 export default LandingPage;
