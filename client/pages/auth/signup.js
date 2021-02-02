cc68304618e41e20d802bb4d5a5cf4fb92a1b597
Perry <perry@jamdirect.ca>
2021-02-02 10:00:03 -0500

    Added sign up error display

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -5,15 +5,20 @@ export default () => {
 
   const [ email, setEmail ] = useState('');
   const [ password, setPassword ] = useState('');
+  const [ errors, setErrors ] = useState([]);
 
   const onSubmit = async (event) => {
     event.preventDefault();
+    try {
+      const response = await axios.post('/api/users/signup', {
+        email, password
+      });
+  
+      console.log(response.data)
 
-    const response = await axios.post('/api/users/signup', {
-      email, password
-    });
-
-    console.log(response.data)
+    } catch (err) {
+      setErrors(err.response.data.errors)
+    }
   }
 
   return (
@@ -27,6 +32,12 @@ export default () => {
         <label>Password</label>
         <input value={password} onChange={ e => setPassword(e.target.value)} type="password" className="form-control" />
       </div>
+      {errors.length > 0  && <div className="alert alert-danger">
+        <h4>Oooops....</h4>
+        <ul className="my-0">
+          {errors.map( err => <li key={err.message}>{err.message}</li>)}
+        </ul>
+      </div>}
       <button className="btn btn-primary">Sign Up</button>
     </form>
   );
