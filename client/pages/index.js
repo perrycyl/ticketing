010a6d111b82a6ee0425f1c14ee4578783229429
Perry <perry@jamdirect.ca>
2021-02-02 15:03:30 -0500

    Added header to pages. Updated _app to handle getInitialProps and alleviate for assigning it to _app in children. Cleanup.

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,17 +1,16 @@
 import buildClient from "../api/build-client"
 
 const LandingPage = ({currentUser}) => {
-    console.log("current user: ", currentUser)
-    return <h1 > Landing Page </h1>;
+    return currentUser ? (
+        <h1>You are signed in</h1>
+    ) : (
+        <h1>You are not signed in.</h1>
+    )
 }
 
 LandingPage.getInitialProps = async (context) => {
     const { data } = await buildClient(context).get('/api/users/currentuser')
-    console.log("DATA: ", data)
     return data;
 };
 
 export default LandingPage;
-
-//internal url for getInitialProps to use KNginx to route reqests.
-//http://ingress-nginx-controller-admission.ingress-nginx.svc.cluster.local
