38b5a8211542400f4791e70a0076d99017fa2f5e
Perry <perry@jamdirect.ca>
2021-02-02 13:27:59 -0500

    Added get initial props for handling index page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,26 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage = ({currentUser}) => {
     return <h1 > Landing Page </h1>;
 }
+
+LandingPage.getInitialProps = async ({req}) => {
+    console.log("req", req.headers)
+    if (typeof window === 'undefined'){
+        const {data} = await axios.get(
+            'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', 
+            {
+                headers: req.headers
+            }
+        );
+        return data
+    } else {
+        const {data} = await axios.get('/api/users/currentuser');
+        return data
+    }
+};
+
+export default LandingPage;
+
+//internal url for getInitialProps to use KNginx to route reqests.
+//http://ingress-nginx-controller-admission.ingress-nginx.svc.cluster.local
