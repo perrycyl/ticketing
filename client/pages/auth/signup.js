14c9532437fc5b8800d191ce7b9ff03fcb407d02
Perry <perry@jamdirect.ca>
2021-02-02 09:38:29 -0500

    Added signup front end code.

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,14 +1,31 @@
+import { useState } from 'react';
+import axios from 'axios';
+
 export default () => {
+
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await axios.post('/api/users/signup', {
+      email, password
+    });
+
+    console.log(response.data)
+  }
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h1>Sign Up</h1>
       <div className="form-group">
         <label>Email Address</label>
-        <input className="form-control" />
+        <input value={email} onChange={ e => setEmail(e.target.value)} className="form-control" />
       </div>
       <div className="form-group">
         <label>Password</label>
-        <input type="password" className="form-control" />
+        <input value={password} onChange={ e => setPassword(e.target.value)} type="password" className="form-control" />
       </div>
       <button className="btn btn-primary">Sign Up</button>
     </form>
