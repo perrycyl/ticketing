cbea04d98cdd0715b0766e6e039d802bcfc54d80
Perry <perry@jamdirect.ca>
2021-02-21 14:49:30 -0500

    Added listener and publisher classes. Hooked classes to listerner and publisher replacing independent functions.

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,43 +1,23 @@
-import nats, { Message } from 'node-nats-streaming';
+import nats from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
-console.clear()
+import { TicketCreatedListener } from './events/ticket-created-listener';
+
+console.clear();
 
 const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
-    url: 'http://localhost:4222'
+  url: 'http://localhost:4222',
 });
 
 stan.on('connect', () => {
-    console.log('Listener connected to NATS');
+  console.log('Listener connected to NATS');
 
-    stan.on('close',() => {
-        console.log('NATS connection closed!');
-        process.exit();
-    })
+  stan.on('close', () => {
+    console.log('NATS connection closed!');
+    process.exit();
+  });
 
-    // options are chained on.
-    const options = stan
-        .subscriptionOptions()
-        .setManualAckMode(true)
-        .setDeliverAllAvailable()
-        .setDurableName('accounting-service');
+  new TicketCreatedListener(stan).listen();
+});
 
-    const subscription = stan.subscribe(
-        'ticket:created',
-        'orders-service-queue-group',
-        options
-        );
-
-    subscription.on('message', (msg: Message) => {
-        // console.log('Message received', msg.getSequence(), msg.getData());
-        const data = msg.getData();
-
-        if (typeof data == 'string'){
-            console.log(`Received event #${msg.getSequence()}, with data: ${data}`)
-        }
-
-        msg.ack();
-    });
-})
-
-process.on('SIGINT', () => stan.close()); // Mac specific
-process.on('SIGTERM', () => stan.close()); // Mac specific
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
